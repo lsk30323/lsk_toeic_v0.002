195774afc0ff9cb95a79c73703854ac1535c1fde
lsk30323 <seungkilee0307@gmail.com>
2026-05-04 16:34:13 +0900

    Add Firebase Authentication plugin configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.toeicapp',
   appName: 'ToeicApp',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com'],
+    },
+  },
 };
 
 export default config;
